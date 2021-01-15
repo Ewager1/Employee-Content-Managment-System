0ab640f87f161f27e7b35ce224e95838e7d8d4cf
EWager <ethanbwager@gmail.com>
2021-01-15 14:30:10 -0500

    created an updated employee role inquiry

--- assets/questions.js
+++ assets/questions.js
@@ -3,7 +3,7 @@ const options = [
     type: "list",
     name: "userAction",
     message: "What would you like to do?",
-    choices: ["View employees by department", "View employees by role",  "Create New Department", "Create New Role", "Create New Employee", "Delete Employee"],
+    choices: ["View employees by department", "View employees by role", "View Roles", "View Departnments", "Create New Department", "Create New Role", "Create New Employee", "Update Employee Role", "Delete Employee"],
   }
 ] 
 
@@ -37,9 +37,49 @@ const terminateEmployee = {
   message: "Enter the Id of the employee you would like to terminate", 
 }
 
+const createRoleQuestions = [
+  {
+    type: "input",
+    name: "roleName",
+    message: "What is name of the new role?",
+  },
+  {
+    type: "input",
+    name: "roleSalary",
+    message: "What is salary of the new role?",
+  },
+  {
+    type: "input",
+    name: "department",
+    message: "What department does the new role belong to?",
+  }
+]
+
+const createDepartmentQuestions = {
+  type: "input",
+  name: "departmentName",
+  message: "What is the name of the new Department?", 
+}
+
+const updateEmployeeRole = [{
+  type: "input",
+  name: "updateEmployeeRole",
+  message: "What is the id of the employee whose role you wish to update?", 
+},
+{
+  type: "input",
+  name: "newEmployeeRole",
+  message: "What is the RoleId of the new role?", 
+}]
+
+
+
+
 //export used in app.js
 module.exports.options = options
 module.exports.createEmployeeQuestions = createEmployeeQuestions
 module.exports.terminateEmployee = terminateEmployee
-
+module.exports.createRoleQuestions = createRoleQuestions
+module.exports.createDepartmentQuestions = createDepartmentQuestions
+module.exports.updateEmployeeRole = updateEmployeeRole
 
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const inquirer = require("inquirer"); //allows use of inquirer npm adding question functionality
 const mysql = require("mysql"); //connects to database
 const nodemon = require("nodemon"); //keeps server refreshing
+const { throwError } = require("rxjs");
 const questions = require("./assets/questions");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -17,16 +18,36 @@ connection.connect(function (err) {
   readAllData();
 });
 
-// start program
+function readAllData() {
+  connection.query(
+    `
+  SELECT empl_id as id, empl_first_name as first_name, empl_last_name as last_name, 
+  role_title as title, dep_name as department, role_salary as salary, manager_id as manager FROM cms.employee_role 
+  LEFT JOIN cms.employee ON employee_role.id=employee.role_id 
+  LEFT JOIN cms.department ON department.id=employee_role.department_id 
+  WHERE empl_first_name IS NOT NULL;
+  `,
+    function (err, result) {
+      console.table(result);
+      options();
+    }
+  );
+}
 
-function optionsPrompt() {
+function options() {
   inquirer.prompt(questions.options).then(function (answer) {
     switch (answer.userAction) {
       case "View employees by department":
-        viewByDepartment();
+        viewByDepartments();
         break;
       case "View employees by role":
         viewByRole();
+        break;
+      case "View Roles":
+        viewRoles();
+        break;
+      case "View Departnments":
+        viewDepartments();
         break;
       case "Create New Department":
         createDepartment(answer.departmentName);
@@ -37,6 +58,9 @@ function optionsPrompt() {
       case "Create New Employee":
         createEmployee();
         break;
+      case "Update Employee Role":
+        updateEmployeeRole();
+        break;
       case "Delete Employee":
         deleteEmployee();
         break;
@@ -44,26 +68,8 @@ function optionsPrompt() {
   });
 }
 
-//**Reading data from database**
-
-//displays all employee data on screen at start of application
-function readAllData() {
-  connection.query(
-    `
-  SELECT empl_id as id, empl_first_name as first_name, empl_last_name as last_name, 
-  role_title as title, dep_name as department, role_salary as salary, manager_id as manager FROM cms.employee_role 
-  LEFT JOIN cms.employee ON employee_role.id=employee.role_id 
-  LEFT JOIN cms.department ON department.id=employee_role.department_id 
-  WHERE empl_first_name IS NOT NULL;
-  `,
-    function () {
-      optionsPrompt();
-    }
-  );
-}
-
-//Returns employee view by Department
-function viewByDepartment() {
+//View by deparments
+function viewByDepartments() {
   connection.query(
     `SELECT empl_id as id, empl_first_name as first_name, empl_last_name as last_name, 
   role_title as title, dep_name as department, role_salary as salary, manager_id as manager FROM cms.employee_role 
@@ -73,12 +79,12 @@ function viewByDepartment() {
     function (err, result) {
       if (err) throw err;
       console.table(result);
-      optionsPrompt();
+      options();
     }
   );
 }
 
-//Returns employee data by Role
+//View employees by Role
 function viewByRole() {
   connection.query(
     `SELECT empl_id as id, empl_first_name as first_name, empl_last_name as last_name, 
@@ -89,30 +95,115 @@ function viewByRole() {
     function (err, result) {
       if (err) throw err;
       console.table(result);
-      optionsPrompt();
+      options();
     }
   );
 }
 
-//Create New Section
+//view Roles
+function viewRoles() {
+  connection.query(`SELECT * FROM cms.employee_role;`, function (err, result) {
+    if (err) throw err;
+    console.table(result);
+    options();
+  });
+}
+
+//Returns Roles
+function viewDepartments() {
+  connection.query(`SELECT * FROM cms.department;`, function (err, result) {
+    if (err) console.log(error);
+    console.table(result);
+    options();
+  });
+}
+
 function createEmployee() {
   inquirer.prompt(questions.createEmployeeQuestions).then(function (answer) {
     if ((answer.employeeManagerId = "undefined")) {
       answer.employeeManagerId = 0;
     }
     connection.query(
-      `INSERT INTO cms.employee (empl_first_name, empl_last_name, role_id, manager_id) VALUES
-       ('${answer.employeeFirstName}',
-       '${answer.employeeLastName}',
-        '${answer.employeeRoleId}',
-        '${answer.employeeManagerId}'
-        );`,
-      function (error, results, fields) {
-        viewByDepartment();
+      "INSERT INTO `cms`.`employee` (`empl_first_name`, `empl_last_name`, `role_id`, `manager_id`) VALUES ('" +
+        answer.employeeFirstName +
+        "', '" +
+        answer.employeeLastName +
+        "', '" +
+        answer.employeeRoleId +
+        "','" +
+        answer.employeeManagerId +
+        "');",
+      function (error, results) {
+        if (error) throwError;
+        viewByDepartments();
+        options();
       }
     );
   });
 }
+
+//creates a new role
+function createRole() {
+  inquirer.prompt(questions.createRoleQuestions).then(function (answer) {
+    connection.query(
+      "INSERT INTO `cms`.`employee_role` (`role_title`, `role_salary`, `department_id`) VALUES ('" +
+        answer.roleName +
+        "', '" +
+        answer.roleSalary +
+        "', '" +
+        answer.department +
+        "');",
+      function (error, results) {
+        if (error) console.log(error);
+        viewRoles();
+        options();
+      }
+    );
+  });
+}
+
+//creates a new Department
+function createDepartment() {
+  inquirer.prompt(questions.createDepartmentQuestions).then(function (answer) {
+    connection.query(
+      "INSERT INTO `cms`.`department` (`dep_name`) VALUES ('" +
+        answer.departmentName +
+        "');",
+      function (error, results) {
+        if (error) console.log(error);
+        viewDepartments();
+        options();
+      }
+    );
+  });
+}
+
+//update Employee Role
+function updateEmployeeRole() {
+  connection.query(
+    //pull a list of employees and their id's for user reference
+    `SELECT empl_id as id, empl_first_name as first_name, empl_last_name as last_name, 
+  role_title as title, dep_name as department, role_salary as salary, manager_id as manager FROM cms.employee_role 
+  LEFT JOIN cms.employee ON employee_role.id=employee.role_id 
+  LEFT JOIN cms.department ON department.id=employee_role.department_id 
+  WHERE empl_first_name IS NOT NULL ORDER BY title ;`,
+    function (error, results, feilds) {
+      console.table(results);
+      //then call  inquirer to ask user to choose which id to terminate an employee
+      inquirer.prompt(questions.updateEmployeeRole).then(function (answer) {
+        connection.query(
+          //pull a list of employees and their id's for user reference
+          "UPDATE `cms`.`employee` SET `role_id` = '" + answer.newEmployeeRole +  "' WHERE (`empl_id` = '"+ answer.updateEmployeeRole +"');",
+          function (error, results, feilds) {
+            if(error) console.log(error)
+           viewByRole()
+            options() 
+    
+      
+      });
+    }
+  );
+})}
 
 //Delete Employee
 function deleteEmployee() {
@@ -120,12 +211,12 @@ function deleteEmployee() {
     //pull a list of employees and their id's for user reference
     `SELECT empl_id AS id, empl_first_name, empl_last_name FROM cms.employee;`,
     function (error, results, feilds) {
-      console.table(results)
+      console.table(results);
       //then call  inquirer to ask user to choose which id to terminate an employee
       inquirer.prompt(questions.terminateEmployee).then(function (answer) {
         results.forEach((element) => {
           employeeId = parseInt(answer.employeeNames);
-      //matches the answer to the correct employee to delete
+          //matches the answer to the correct employee to delete
           if (element.id === employeeId) {
             connection.query(
               `DELETE from cms.employee where empl_id = ${employeeId}`
@@ -133,7 +224,8 @@ function deleteEmployee() {
           }
         });
         //resets program
-        viewByDepartment();
+        viewByDepartments();
+        options();
       });
     }
   );
